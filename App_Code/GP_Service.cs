ba2d660846499069995363632f32419d6d4a563c
TenilleF19 <tjsforbes@gmail.com>
2020-08-13 14:19:37 +0200

    feat: add login function to service

---
+++ App_Code/GP_Service.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "GP_Service" in code, svc and config file together.
+public class GP_Service : IGP_Service
+{
+    //connect to dbml
+    DataClassesDataContext db = new DataClassesDataContext();
+
+    public int login(string email, string password)
+    {
+        //check if user information is in the database
+        var user = (from u in db.Users
+                    where u.Email.Equals(email) && u.Password.Equals(password)
+                    select u).FirstOrDefault();
+
+
+        if(user != null)
+        {
+            //if user with given email and password exists, return their ID
+            return user.ID;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+
+}
